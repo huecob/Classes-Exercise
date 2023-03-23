5b742cb456673c8221dc8372c7ba264d5405b33f
Jake Pese <jpiquepese@gmail.com>
2023-03-22 21:52:27 -0700

    Final Code for Harvest Done

--- harvest.py
+++ harvest.py
@@ -55,6 +55,9 @@ def make_melon_types():
 
     return all_melon_types
 
+print(make_melon_types)
+#returns a list of current melon types
+
 melon_types = make_melon_types()
 
 def print_pairing_info(melon_types):
@@ -93,14 +96,77 @@ class Melon:
     # Fill in the rest
     # Needs __init__ and is_sellable methods
 
+    def __init__(self, type, shape, color, field_number, harvester):
 
-def make_melons(melon_types):
+        self.type = type
+        self.shape = shape
+        self.color = color
+        self.field_number = field_number
+        self.harvester = harvester
+
+    def is_sellable(self):
+        if self.shape_rating > 5 and self.color_rating > 5 and self.field != 3:
+            return True
+        else:
+            return False
+
+
+def make_melons(melon_types): 
     """Returns a list of Melon objects."""
 
-    # Fill in the rest
+    #musk = MelonType("musk","Muskmelon",1998,"green",True,True)
+    #where is our melon data? make melon type look up function... 
 
+    retval = []
+
+    melon_data = make_melon_type_lookup(melon_types) #this gives us a list of dictionary objects
+
+    melon1 = Melon(melon_data["yw"], 8,7,2,"Shiela")
+    retval.append(melon1)
+
+    melon2 = Melon(melon_data["yw"],3,4,2,"Shiela")
+    retval.append(melon2)
+
+    melon3 = Melon(melon_data["yw"],9,8,3,"Shiela")
+    retval.append(melon3)
+
+    melon4 = Melon(melon_data["cas"],10,6,35,"Shiela")
+    retval.append(melon4)
+
+    melon5 = Melon(melon_data["cren"],8,9,35,"Michael")
+    retval.append(melon5)
+
+    melon6 = Melon(melon_data["cren"],8,2,35,"Michael")
+    retval.append(melon6)
+
+    melon7 = Melon(melon_data["cren"],2,3,4,"Michael")
+    retval.append(melon7)
+
+    melon8 = Melon(melon_data["musk"],6,7,4,"Michael")
+    retval.append(melon8)
+
+    melon9 = Melon(melon_data["yw"],7,10,3,"Shiela")
+    retval.append(melon9)
+
+    return retval
+
+melons = make_melons(melon_types)
 
 def get_sellability_report(melons):
     """Given a list of melon object, prints whether each one is sellable."""
 
+    for elements in melons:
+        harvester = f"Harvested by {elements.harvester}"
+        field = f"Field #{elements.field}"
+
+        if elements.is_sellable():
+            sellable = "CAN BE SOLD"
+        else:
+            sellable = "NOT SELLABLE"
+        
+        print(f"{harvester} from {field} {sellable}")
+
     # Fill in the rest
+
+#you can fix part one by looking on line 58
+#validate 105 with 140
